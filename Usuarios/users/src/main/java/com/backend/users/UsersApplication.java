085cf3753e85053a69ffac228b3d43c577055f59
zamir5895 <zamir.lizardo@utec.edu.pe>
2025-04-06 23:18:02 -0500

    mmmm

--- Usuarios/users/src/main/java/com/backend/users/UsersApplication.java
+++ Usuarios/users/src/main/java/com/backend/users/UsersApplication.java
@@ -2,11 +2,20 @@ package com.backend.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class UsersApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load(); 
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        System.setProperty("SUPPORT_EMAIL", dotenv.get("SUPPORT_EMAIL"));
+        System.setProperty("APP_PASSWORD", dotenv.get("APP_PASSWORD"));
         SpringApplication.run(UsersApplication.class, args);
     }
 
--- Usuarios/users/src/test/java/com/backend/users/UsersApplicationTests.java
+++ Usuarios/users/src/test/java/com/backend/users/UsersApplicationTests.java
@@ -1,13 +1,27 @@
 package com.backend.users;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.junit.jupiter.api.BeforeAll;
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootTest
 class UsersApplicationTests {
 
+   
+    @BeforeAll
+    static void setupEnv() {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("JWT_SECRET_KEY", dotenv.get("JWT_SECRET_KEY"));
+        System.setProperty("SUPPORT_EMAIL", dotenv.get("SUPPORT_EMAIL"));
+        System.setProperty("APP_PASSWORD", dotenv.get("APP_PASSWORD"));    
+    }
     @Test
     void contextLoads() {
     }
+
 
 }
